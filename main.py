f40cf76e3b9d8e1d28e5d70d006e421c51923628
OdalaT <>
2021-06-01 10:26:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+'''
+data types
+'''
+# s="fr"
+# print(s)
+# print(type(s))
+
+# =============================================
+#find character by index
+# s= "string sample"
+# print (s[5])
+
+# #slicing
+# print(s[0:6])
+
+#immutable
+# s= "string sample"
+# s[2]= 'o'
+# print(s[2])
+
+# '''
+# # Sequence type (List Tuple range)
+# # '''
+# # x= [10, 2.5, 'hello']
+# # print(x[1:3]) 
+# # '''
+# # Lists are mutable
+# # '''
+# # x[2]= 'python'
+# # print(x)
+
+# '''
+# tuple
+# '''
+# # tuple = ()
+# tuple = ('donkey', [3,2,1], (4,3,2))
+# tuple[2]= 10
+# print(tuple)
+
+# '''
+# range
+# '''
+# x= range(11)
+# for n in x:
+#   print (n)
+
+# '''
+# Mappoing type dict
+
+# '''
+
+# dict = {'name': 'Odala', 'age': 42}
+# print(dict['age'])
+# print(dict.get('name'))
+# dict['age']= 34
+# print(dict)
+
+# '''
+# SEt data types
+# '''
+# set= {5.3, "hi", (1,2,3), [4,3,2]}
+# print(set)
+
+'''
+boolean type
+'''
+# print(type(True))
+# print(True==1)
+# print(False==0)
+# print(True+True)
+
+print(False or True)
